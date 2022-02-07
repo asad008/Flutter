908f9644a4fabca10b4ecf9672a0b8134012992d
Asadujjaman <asadujjamanasadcse5@gmail.com>
2022-02-08 00:28:07 +0600

    Dummy product shown

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,19 +1,30 @@
 // ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers
 
+import 'package:catalog_app/models/catalog.dart';
 import 'package:catalog_app/widgets/drawer.dart';
 import 'package:flutter/material.dart';
 
+import '../widgets/item_widget.dart';
+
 class HomePage extends StatelessWidget {
+  final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text("Shopping App"),
         ),
-        body: Center(
-          child: Container(
-            
-            child: Text("Hello Asadujjaman"),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: ListView.builder(
+            // itemCount: CatalogModel.items.length,
+            itemCount: dummyList.length,
+            itemBuilder: (context, index) {
+              return ItemWidget(
+                //item: CatalogModel.items[index],
+                item: dummyList[index],
+              );
+            },
           ),
         ),
         drawer: MyDrawer());
